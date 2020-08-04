be0898e82a6d7544151d4b4525077fa41813bf61
supreeth1816 <supreet.venkata@gmail.com>
2020-08-05 02:58:22 +0530

    Initial Commit

---
+++ meditationApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  meditationApp
+//
+//  Created by Supreeth on 05/08/20.
+//  Copyright © 2020 Supreeth. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
